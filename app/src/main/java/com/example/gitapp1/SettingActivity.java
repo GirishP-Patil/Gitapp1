6bdb537fe8f08fc5ea747b05004d3932bb28f86f
Girish <girishpatil.java@gmail.com>
2021-12-03 11:35:02 +0530

    adding Setting for app

---
+++ app/src/main/java/com/example/gitapp1/SettingActivity.java
@@ -0,0 +1,23 @@
+package com.example.gitapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingActivity extends AppCompatActivity {
+
+
+	// adding more feature here for new release
+	Textview textview;
+	Textview textbackground;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setwallpaper(View view) {
+    }
+}
